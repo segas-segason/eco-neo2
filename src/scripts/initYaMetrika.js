a8467d7da93a9b1670138b517da339456495e27b
Sergei <segas.segason@gmail.com>
2026-03-10 17:18:44 +0300

    Перемещение подключения из html в main.js сервисов яндекса

---
+++ src/scripts/initYaMetrika.js
@@ -0,0 +1,34 @@
+export function initYaMetrika() {
+    (function (m, e, t, r, i, k, a) {
+        m[i] =
+            m[i] ||
+            function () {
+                (m[i].a = m[i].a || []).push(arguments);
+            };
+
+        m[i].l = Date.now();
+
+        for (let j = 0; j < document.scripts.length; j += 1) {
+            if (document.scripts[j].src === r) {
+                return;
+            }
+        }
+
+        k = e.createElement(t);
+        a = e.getElementsByTagName(t)[0];
+        k.async = 1;
+        k.src = r;
+        a.parentNode.insertBefore(k, a);
+    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=107190957', 'ym');
+
+    window.ym(107190957, 'init', {
+        ssr: true,
+        webvisor: true,
+        clickmap: true,
+        ecommerce: 'dataLayer',
+        referrer: document.referrer,
+        url: location.href,
+        accurateTrackBounce: true,
+        trackLinks: true,
+    });
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,13 @@ import { initBackdropMenu } from './animations/backdropMenu';
 import { initPopup } from './animations/popup';
 import { initAccordionFaq } from './animations/accordionFaq';
 import { initLayoutTabs } from './layoutTabs';
+import { initYaMap } from './initYaMap';
+import { initYaMetrika } from './initYaMetrika';
 
 initMobileMenu();
 initBackdropMenu();
 initPopup();
 initAccordionFaq();
 initLayoutTabs();
+initYaMap();
+initYaMetrika();
